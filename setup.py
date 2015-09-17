241f5ae4a7de4768471d70802c28ecdba066d1ce
Sam Stavinoha <smlstvnh@gmail.com>
2015-09-16 21:14:55 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+
+INSTALL_REQUIRES = [
+    'requests',
+]
+
+
+TESTS_REQUIRE = [
+    'nosetests>=1.3.7',
+]
+
+package_attributes = {
+    'author': 'Sam Stavionha',
+    'author_email': 'smlstvnh@gmail.com',
+    
+    'name': 'circleci_sandbox',
+    'description': 'See what circleci will do.',
+    'install_requires': INSTALL_REQUIRES,
+    'py_modules': ['hi'],
+    'scripts': ['hi.py'],
+    'tests_require': TESTS_REQUIRE,
+    'url': 'https://github.com/samstav/circleci-sandbox',
+    'version': 1.0.0.,
+}
+
+
+setup(**package_attributes)
